afb31a2de951bb392dcec6d028e8a15f0292864d
Andres Brizuela <andres.brizuela@BP4.com>
2019-11-05 21:05:11 -0300

    config hmr

--- src/main.ts
+++ src/main.ts
@@ -12,19 +12,4 @@ if ( environment.production )
 
 const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
 
-if ( environment.hmr )
-{
-    if ( module['hot'] )
-    {
-        hmrBootstrap(module, bootstrap);
-    }
-    else
-    {
-        console.error('HMR is not enabled for webpack-dev-server!');
-        console.log('Are you using the --hmr flag for ng serve?');
-    }
-}
-else
-{
-    bootstrap().catch(err => console.log(err));
-}
+bootstrap().catch(err => console.log(err));
