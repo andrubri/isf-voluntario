1108fd449d6ade82397bd826fd8a32a320b0a993
TomasReisenman <tom.manrey@gmail.com>
2019-11-05 23:41:38 -0300

    added pop up for creating user from persona

--- src/app/main/page/personas/personas.component.ts
+++ src/app/main/page/personas/personas.component.ts
@@ -1,24 +1,23 @@
-import {Component, ElementRef, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 
 import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
-import {fuseAnimations} from '../../../../@fuse/animations';
-import {MatDialog, MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
-import {FuseProgressBarService} from '../../../../@fuse/components/progress-bar/progress-bar.service';
-import {ISFService} from '../../../services/isf.service';
-import {fromEvent} from 'rxjs';
-import {AccionConfirmarComponent} from '../../modal/AccionConfirmar/accionconfirmar.component';
+import { fuseAnimations } from '../../../../@fuse/animations';
+import { MatDialog, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { FuseProgressBarService } from '../../../../@fuse/components/progress-bar/progress-bar.service';
+import { ISFService } from '../../../services/isf.service';
+import { fromEvent } from 'rxjs';
+import { AccionConfirmarComponent } from '../../modal/AccionConfirmar/accionconfirmar.component';
 
 @Component({
-    selector   : 'personas',
+    selector: 'personas',
     templateUrl: './personas.component.html',
-    styleUrls  : ['./personas.component.scss'],
-    animations   : fuseAnimations,
+    styleUrls: ['./personas.component.scss'],
+    animations: fuseAnimations,
     encapsulation: ViewEncapsulation.None
 })
-export class PersonasComponent implements OnInit
-{
+export class PersonasComponent implements OnInit {
     public dataSource: MatTableDataSource<any>;
-    public displayedColumns = ['nombre','apellido', 'estado',  'ciudadResidencia', 'provinciaResidencia','email','dieta','ocupacion', 'accion'];
+    public displayedColumns = ['nombre', 'apellido', 'estado', 'ciudadResidencia', 'provinciaResidencia', 'email', 'dieta', 'ocupacion', 'accion', 'user'];
     public perfiles: [];
 
     @ViewChild(MatPaginator)
@@ -40,22 +39,19 @@ export class PersonasComponent implements OnInit
         private _fuseProgressBarService: FuseProgressBarService,
         private _isfService: ISFService,
         private _dialog: MatDialog
-    )
-    {
+    ) {
         this._fuseProgressBarService.show();
         this.perfiles = [];
     }
 
-    async ngOnInit()
-    {
+    async ngOnInit() {
         const data = await this._isfService.getAllPersonas();
         this.dataSource = new MatTableDataSource(data);
         this.dataSource.sort = this.sort;
         this.dataSource.paginator = this.paginator;
         fromEvent(this.filter.nativeElement, 'keyup')
             .subscribe(() => {
-                if ( !this.dataSource )
-                {
+                if (!this.dataSource) {
                     return;
                 }
                 this.dataSource.filter = this.filter.nativeElement.value;
@@ -72,15 +68,43 @@ export class PersonasComponent implements OnInit
         });
     }
 
-    async remove(idPersona: number){
+    async remove(idPersona: number) {
         this.openDialogConfirm({
             etiqueta: "AccionOpciones",
             titulo1: "¿Esta seguro que desea eliminar ",
-            titulo2: "la actividad?",
+            titulo2: "la persona?",
             txtBoton: "Eliminar",
             callback: async () => {
                 this._fuseProgressBarService.show();
                 await this._isfService.removePersona(idPersona);
+                this.ngOnInit();
+            },
+            altoModal: "300px",
+            anchoModal: "450px"
+        });
+    }
+    async createUser(persona: any) {
+
+        const user = {
+            apellido: persona.apellido,
+            email: persona.email,
+            idPersona: persona.idPersona,
+            nombre: persona.nombre,
+            clave: 'C4mb14m3'
+        }
+        this.openDialogConfirm({
+            etiqueta: "AccionOpciones",
+            titulo1: "¿Esta seguro que desea crear ",
+            titulo2: "usuario para la persona?",
+            txtBoton: "Crear",
+            callback: async () => {
+                this._fuseProgressBarService.show();
+                try {
+                    await this._isfService.addUser(user);
+
+                } catch (error) {
+                    alert('User already exists')
+                }
                 this.ngOnInit();
             },
             altoModal: "300px",
