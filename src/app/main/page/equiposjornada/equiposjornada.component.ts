fa9d98a93d874eecb99203deb547ff55592ea42e
Andres Brizuela <andres.brizuela@BP4.com>
2019-12-01 23:51:42 -0300

    Fix Jornadas

--- src/app/main/page/equiposjornada/equiposjornada.component.ts
+++ src/app/main/page/equiposjornada/equiposjornada.component.ts
@@ -1,14 +1,14 @@
+import {Component, OnDestroy, OnInit, ViewChild, ViewEncapsulation, Input} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {MatDialog, MatPaginator, MatSnackBar, MatSort, MatTableDataSource} from '@angular/material';
 
-import { Component, OnDestroy, OnInit, ViewChild, ViewEncapsulation, Input } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { MatDialog, MatPaginator, MatSnackBar, MatSort, MatTableDataSource } from '@angular/material';
-
-import { fuseAnimations } from '@fuse/animations';
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
-import { ISFService } from '../../../services/isf.service';
-import { FuseProgressBarService } from '../../../../@fuse/components/progress-bar/progress-bar.service';
-import { AddvoluntarioComponent } from '../../modal/AddVoluntario/addvoluntario.component';
+import {fuseAnimations} from '@fuse/animations';
+import {Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
+import {ISFService} from '../../../services/isf.service';
+import {FuseProgressBarService} from '../../../../@fuse/components/progress-bar/progress-bar.service';
+import {AddvoluntarioComponent} from '../../modal/AddVoluntario/addvoluntario.component';
+import {isObject} from "util";
 
 @Component({
     selector: 'formequipos',
@@ -54,10 +54,10 @@ export class EquiposJornadaComponent implements OnInit, OnDestroy {
     async ngOnInit(): Promise<void> {
         if (this._route.snapshot.paramMap.get('jornada')) {
             const idAct = Number(this._route.snapshot.paramMap.get('jornada'));
-            this.jornada = await this._isfService.getJornadaById(idAct)
+            this.jornada = await this._isfService.getJornadaById(idAct);
             this.equipo = await this._isfService.getEquipoById(this.jornada.idEquipo);
             this.dataPersonas.data = await this._isfService.getPersonasJornadaAct(idAct);
-            this.personas_act = await this._isfService.getPersonasAct(this.jornada.idEquipo)
+            this.personas_act = await this._isfService.getPersonasAct(this.jornada.idEquipo);
             this.pageType = 'edit';
         } else {
             this._router.navigate(['equipos']);
@@ -89,24 +89,54 @@ export class EquiposJornadaComponent implements OnInit, OnDestroy {
             label: 'Voluntario',
             items: this.personas_act,
             callback: async (item) => {
-                const info = this.dataPersonas.data;
-                if (this.equipo.idEquipo) {
-                    const newItem: any = this._isfService.addPersonaJornada(this.jornada.idJornadas, item.idPersona);
+                try {
+                    const info = this.dataPersonas.data;
+                    await this._isfService.addPersonaJornada(this.jornada.idJornadas, item.idPersona);
+                    const newItem: any = {};
                     newItem.nombre = item.Persona.nombre;
                     newItem.apellido = item.Persona.apellido;
                     info.push(newItem);
-                } else {
-                    info.push(item);
+                    this.dataPersonas.data = info;
+                } catch (e) {
+                    if (e.error && !isObject(e.error)) {
+                        this._matSnackBar.open(e.error, 'Aceptar', {
+                            verticalPosition: 'top',
+                            panelClass: 'errorSnackBar',
+                            duration: 2000
+                        });
+                    } else {
+                        this._matSnackBar.open('Ocurrio un error al confirmar el voluntario!. Intente más tarde.', 'Aceptar', {
+                            verticalPosition: 'top',
+                            panelClass: 'errorSnackBar',
+                            duration: 2000
+                        });
+                    }
                 }
-                this.dataPersonas.data = info;
+
             },
             altoModal: '300px',
             anchoModal: '450px'
         });
     }
 
-    confAsistencia(idVoluntario: number, valor: any): void {
-        this._isfService.AsistenciaJornada(this.jornada.idJornadas, idVoluntario, valor.checked);
+    async confAsistencia(idVoluntario: number, valor: any): Promise<void> {
+        try {
+            await this._isfService.AsistenciaJornada(this.jornada.idJornadas, idVoluntario, valor.checked);
+        }catch (e) {
+            if (e.error && !isObject(e.error)) {
+                this._matSnackBar.open(e.error, 'Aceptar', {
+                    verticalPosition: 'top',
+                    panelClass: 'errorSnackBar',
+                    duration: 2000
+                });
+            } else {
+                this._matSnackBar.open('Ocurrio un error al guardar la asistencia del voluntario!. Intente más tarde.', 'Aceptar', {
+                    verticalPosition: 'top',
+                    panelClass: 'errorSnackBar',
+                    duration: 2000
+                });
+            }
+        }
     }
 
     getStatus(value: any): boolean {
